13d41fbb355fb84459eab9336a8ff5d375a3aee2
ardy aryan <ardy.aryan@gmail.com>
2016-02-04 00:30:01 -0800

    test 2

--- public/js/partials/dashboard.js
+++ public/js/partials/dashboard.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         if(validate()) {
             $('#graph_loading').show();
             setTimeout(function(){
-                renderChart();
+                getTripsByDriver();
             }, 500);
         }
     });
